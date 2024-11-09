163e3798920bd851607fc8cf381b699643c44558
Marshall <144924070+Marshall-J2698@users.noreply.github.com>
2024-11-09 13:30:12 -0600

    refactored to improve output format

--- multi-init-ftm/multi-init-ftm.ino
+++ multi-init-ftm/multi-init-ftm.ino
@@ -12,13 +12,14 @@
    THIS FEATURE IS SUPPORTED ONLY BY ESP32-S2 AND ESP32-C3
 */
 
-// Change the SSID and PASSWORD here if needed
-const char *WIFI_FTM_SSID1 = "beacon1";  // SSID of AP that has FTM Enabled
-const char *WIFI_FTM_SSID2 = "beacon2";       // STA Password
+// Beacon number and names -- assumes no password to connect
+int numBeacons = 2;
+const char *beacons[2] = { "beacon2", "beacon3" };  //"beacon1",
+
 
 // FTM settings
 // Number of FTM frames requested in terms of 4 or 8 bursts (allowed values - 0 (No pref), 16, 24, 32, 64)
-const uint8_t FTM_FRAME_COUNT = 8; //default 16
+const uint8_t FTM_FRAME_COUNT = 8;  //default 16
 // Requested time period between consecutive FTM bursts in 100’s of milliseconds (allowed values - 0 (No pref) or 2-255)
 const uint16_t FTM_BURST_PERIOD = 2;
 
@@ -29,26 +30,26 @@ bool ftmSuccess = true;
 // bool received = false;
 
 unsigned long startConnectTime;
-int connectTimeOut= 200; //time in ms that we will try connecting for.
+int connectTimeOut = 250;  //time in ms that we will try connecting for.
 
 // FTM report handler with the calculated data from the round trip
 // WARNING: This function is called from a separate FreeRTOS task (thread)!
 void onFtmReport(arduino_event_t *event) {
-  const char *status_str[5] = {"SUCCESS", "UNSUPPORTED", "CONF_REJECTED", "NO_RESPONSE", "FAIL"};
+  const char *status_str[5] = { "SUCCESS", "UNSUPPORTED", "CONF_REJECTED", "NO_RESPONSE", "FAIL" };
   wifi_event_ftm_report_t *report = &event->event_info.wifi_ftm_report;
   // Set the global report status
   ftmSuccess = report->status == FTM_STATUS_SUCCESS;
   if (ftmSuccess) {
     // The estimated distance in meters may vary depending on some factors (see README file)
-    Serial.printf("\n%.2f,%lu,%d\n", (float)report->dist_est / 100.0, report->rtt_est,WiFi.RSSI());
+    Serial.printf("%.2f|%lu|%d", (float)report->dist_est / 100.0, report->rtt_est, WiFi.RSSI());
     // Pointer to FTM Report with multiple entries, should be freed after use
     free(report->ftm_report_data);
-    WiFi.disconnect(); //might be evil!!!
+    WiFi.disconnect();  //might be evil!!!
     // received = true;
   } else {
-    Serial.println("FTM Error: ");
-    // Serial.println(status_str[report->status]);
-    WiFi.disconnect(); //might be evil!!!
+    Serial.print("FTMErr:");
+    Serial.print(status_str[report->status]);
+    WiFi.disconnect();  //might be evil!!!
   }
   // Signal that report is received
   xSemaphoreGive(ftmSemaphore);
@@ -57,7 +58,7 @@ void onFtmReport(arduino_event_t *event) {
 // Initiate FTM Session and wait for FTM Report
 bool getFtmReport() {
   if (!WiFi.initiateFTM(FTM_FRAME_COUNT, FTM_BURST_PERIOD)) {
-    Serial.println("FTM Error: Initiate Session Failed");
+    Serial.print("FTM Error: Initiate Session Failed");
     return false;
   }
   // Wait for signal that report is received and return true if status was success
@@ -72,34 +73,28 @@ void setup() {
 
   // Will call onFtmReport() from another thread with FTM Report events.
   WiFi.onEvent(onFtmReport, ARDUINO_EVENT_WIFI_FTM_REPORT);
+}
 
 void loop() {
-  startConnectTime = millis();
-  Serial.print("\nbeacon1: ");
-  WiFi.begin(WIFI_FTM_SSID1,NULL);
-  while((WiFi.status() != WL_CONNECTED) && (millis()-startConnectTime) < connectTimeOut){
-    // Serial.print("wait-con1");
-    delay(20); // can potentially reduce further to get even faster responses
+  Serial.print("{");
+  for (int i = 0; i < numBeacons; i++) {
+    startConnectTime = millis();
+    if (i != 0){
+      Serial.print(",");
+    }
+      Serial.printf("%s:", beacons[i]);
+    WiFi.begin(beacons[i], NULL);
+    while ((WiFi.status() != WL_CONNECTED) && (millis() - startConnectTime) < connectTimeOut) { 
+      delay(10);
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      getFtmReport();
+      WiFi.disconnect();
+    } else {
+      WiFi.disconnect();
+      Serial.print("conERR");
+    }
   }
-  if(WiFi.status()==WL_CONNECTED){
-    getFtmReport();
-    WiFi.disconnect();
-  }else{
-    WiFi.disconnect();
-  }
-
-  startConnectTime = millis();
-  Serial.print("\nbeacon2: ");
-  WiFi.begin(WIFI_FTM_SSID2,NULL);
-  while((WiFi.status() != WL_CONNECTED) && (millis()-startConnectTime) < connectTimeOut){
-    // Serial.print("wait-con2");
-    delay(20);
-  }
-  if(WiFi.status()==WL_CONNECTED){
-    getFtmReport();
-    WiFi.disconnect();
-  }else{
-    WiFi.disconnect();
-  }
-
+  Serial.print("}");
+  Serial.println();
 }
